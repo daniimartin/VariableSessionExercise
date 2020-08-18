c9ccca8cb9df803009e645aaf80b4b2b685c4840
daniimartin <68691828+daniimartin@users.noreply.github.com>
2020-08-18 12:21:02 +0200

    VariableSessionExercise

---
+++ Tutorial01_Session/Tutorial01_Session/Controllers/SessionController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tutorial01_Session.Controllers
+{
+    public class SessionController : Controller
+    {
+
+        // GET: Index
+        public ActionResult Index(String ingrediente)
+        {
+
+            if (ingrediente != null)
+            {
+                List<String> lista;
+
+                //Almaceno en la session
+                if (Session["PIZZA"] != null)
+                {
+                    lista = Session["PIZZA"] as List<String>;    
+                }
+                else
+                {
+                    //Creamos una nueva lista
+                    lista = new List<string>();
+                }
+
+                lista.Add(ingrediente);
+                //Almacenamos los cambios en la sesión
+                Session["PIZZA"] = lista;
+            }
+
+            return View();
+        }
+
+        //GET: Compra
+        public ActionResult Compra()
+        {
+
+            return View(Session["PIZZA"]);
+        }
+
+        //ActionResult del link de eliminar Session
+        public ActionResult Borrar()
+        {
+            Session["PIZZA"] = null;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
